08d955d6ae59eea8dce5157dede1363ffdaae9b8
bingying1987 <mac@macdeMacBook-Pro.local>
2016-01-04 14:41:13 +0800

    init

---
+++ 雅思英语/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  雅思英语
+//
+//  Created by mac on 16/1/4.
+//  Copyright (c) 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
